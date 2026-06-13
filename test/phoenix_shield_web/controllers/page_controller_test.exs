3d4b6c3cc5e2f63fbf98a623b5dcaf96b68a410d
Jailson Carneiro <jailsoncarneiro@hotmail.com>
2026-06-13 08:55:47 -0300

    chore: remove obsolete page controller test

--- test/phoenix_shield_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PhoenixShieldWeb.PageControllerTest do
-  use PhoenixShieldWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
